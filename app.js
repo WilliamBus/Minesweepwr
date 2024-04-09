3dac8e90ba86e4244b37931cbececf7b92f37d90
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-04-09 14:58:16 +1200

    rl greetings

---
+++ app.js
@@ -0,0 +1,22 @@
+//NOTE
+
+/*
+readline is a JS object (rl)
+*/
+
+//Step 1: include the readline module
+const readline = require('readline')
+
+//Step 2: Create an instance of readline.Interface
+const r1 = readline.Interface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//Step 3: prompt the user for their name
+rl.question('What is your name?', (name) => {
+    //Step 4: Display a greeting
+    console.log(`Hello, ${name}!`)
+})
+//Step 5: Close the readline interface.
+r1.close()
