57817896dc5121f6bf29baabc809598ec3be4ffd
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-04-29 10:42:01 +1200

    changes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 // rl.question('What is your name?', (name) => {
 //     //Step 4: Display a greeting
 //     console.log(`Hello, ${name}!`)
-    
+
 //     //Step 5: Close the readline interface.
 //     r1.close()
 // })
@@ -30,17 +30,17 @@ const readline = require('readline')
 //Create and interface for the input and output.
 const rl = readline.createInterface({
     input: process.stdin,
-    input: process.stdout,
+    output: process.stdout,
 })
 
 //Function to generate a random number between min and max (inclusive).
 function getRandomInt(min, max) {
     min = Math.ceil(min),
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max-min+1)) + min
+        max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-//The secret number to guess between 1 and 100.
+//The secret number to guewidss between 1 and 100.
 const secretNumber = getRandomInt(1, 100)
 
 //Counter for number of attempts.
@@ -55,7 +55,7 @@ function guessNumber() {
         const guess = parseInt(answer, 10) //Converting the input string to a number.
 
         //Check if the guess is correct.
-        if (guess === secretNUmber) {
+        if (guess === secretNumber) {
             console.log(`Correct! the number was ${secretNumber}`)
             rl.close()
         } else if (guess < secretNumber) {
